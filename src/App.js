33ef81958d5a2bc7afd82462a90f07d94fca77c4
Ivan <komar.ioan@gmail.com>
2019-06-02 12:19:44 +0300

    use full form with return

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react"
 
-const App = () => (
+const App = () => {
+    return(
     <div><h1>Hello World!</h1>
     <p> Lorem ipsum dolor sit amet </p>
     </div>
     )
-
+}
 export default App
