d20534097461704198bd3967cd1b8ae662bf283a
Ivan <komar.ioan@gmail.com>
2019-06-02 12:13:10 +0300

    use brackets for improove code look

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 
 function App () {
-    return <div><h1>Hello World!</h1>
+    return (
+    <div><h1>Hello World!</h1>
     <p> Lorem ipsum dolor sit amet </p>
     </div>
+    )
 }
 
 export default App
