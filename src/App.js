83d46085dfc91c8eccfc524d71868b66fe0abb79
Ivan <komar.ioan@gmail.com>
2019-06-02 12:46:42 +0300

    add shablon, use / for solo tags, use className

--- src/App.js
+++ src/App.js
@@ -2,8 +2,49 @@ import React from "react"
 
 const App = () => {
     return(
-    <div><h1>Hello World!</h1>
-    <p> Lorem ipsum dolor sit amet </p>
+    <div>
+        <header className="header">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-3">
+					<div className="logo">
+						<img src="img/logo.png" alt=""/>
+					</div>
+				</div>
+				<div className="col-lg-6">
+					<div className="menu">
+						<ul>
+							<li><a href="">Home</a></li>
+							<li><a href="">Product</a></li>
+							<li><a href="">Payment</a></li>
+							<li><a href="">Shipping</a></li>
+						</ul>
+					</div>
+				</div>
+				<div className="col-lg-3">
+					<div className="cart text-center">
+						<div className="products-count">0</div>
+						<div className="products-price">$500</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main className="main">
+		<div className="container">
+			<div className="row">
+				<div className="col-lg-3">
+					
+				</div>
+				<div className="col-lg-9">
+					
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer className="footer">
+		
+	</footer>
     </div>
     )
 }
