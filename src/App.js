b46336a9728a965efc540b087e0c92c025144a32
Ivan <komar.ioan@gmail.com>
2019-06-02 12:09:18 +0300

    JSX always must return 1 element. for exp - <h1> and <p> in <div>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react"
 
 function App () {
-    return "Hello World!"
+    return <div><h1>Hello World!</h1>
+    <p> Lorem ipsum dolor sit amet </p>
+    </div>
 }
 
 export default App
