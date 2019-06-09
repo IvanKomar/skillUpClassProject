40ff53b33eea0cd87d6d92cc475546ccd2b347f3
Ivan <komar.ioan@gmail.com>
2019-06-09 11:32:41 +0300

    Add prodyctlistitem

---
+++ src/App/Main/Products/ProductListItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ProductListItem = () => {
+
+    return (
+        <div className="product-list-item">
+            <div className="Product title"> Iphone 7 </div>
+            <div className="product-description">This is Iphone 7...</div>
+            <div className="product-features">Type: phone</div>
+            <div className="product-features">Capacity: 64 Gb</div>
+            <div className="product-price"> 500 $</div>
+            <button className="btn-add-to-cart">Add to Cart</button>
+        </div>
+    )
+}
+
+export default ProductListItem
--- src/App/Main/Products/ProductsList.js
+++ src/App/Main/Products/ProductsList.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './ProductsList.css'
+import ProductListItem from './ProductListItem'
 
 const ProductsList = () => {
 
     return (
         <div>
-        <h1 className="page-title">ProductsList</h1>
+            <h1 className="page-title">ProductsList</h1>
+            <div className="row">
+                <div className="col-lg-6">
+                    <ProductListItem/>
+                </div>
+            </div>
         </div>
     )
 }
