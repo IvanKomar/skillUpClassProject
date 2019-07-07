7e1b81eafb47b8b781db05ef83946c1c746ef3c5
Ivan <komar.ioan@gmail.com>
2019-07-07 12:24:07 +0300

    add headerLogo link to main page

--- src/App/Header/HeaderLogo/HeaderLogo.js
+++ src/App/Header/HeaderLogo/HeaderLogo.js
@@ -1,10 +1,13 @@
 import React from "react"
 import logo from './logo.png'
+import {Link} from 'react-router-dom'
 
 const HeaderLogo = () => {
     return (
     <div className="logo">
-        <img src={logo} alt=""/>
+       <Link to='/'>
+           <img src={logo} alt=""/>
+        </Link>  
     </div>
     )
 }           
