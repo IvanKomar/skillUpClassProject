833a99dc841adcc16c3d754b00ba76b6db6772fc
Ivan <komar.ioan@gmail.com>
2019-07-27 12:54:23 +0300

    use global state to our App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux'
 import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
+const productLikeReducer = (state={
+    '1': true,
+    '2': true,
+},action)=> state
+const store = createStore(productLikeReducer) 
+// reducer - функция яка буде знати як змінювати стейт
+// в createStore потрібно передати reducer productLikeReducer на базі попереднього стейту та екшну
+
 ReactDOM.render(
-<BrowserRouter>
-<App />
-</BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+                <App />
+        </BrowserRouter>
+    </Provider>
+
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
